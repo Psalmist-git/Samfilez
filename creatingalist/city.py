4153fbef660160f31e810ef72a98e39dee53c5c9
ogah samson <ogahsamsonogah@gmail.com>
2026-09-11 16:21:42 +0100

    creatingslist

---
+++ creatingalist/city.py
@@ -0,0 +1,20 @@
+city = ['Abuja', 'Kaduna', 'Nassarawa', 'Jos']
+print(city)
+
+city.sort()
+print(city)
+
+city.sort(reverse=True)
+print(city)
+
+city = ['Abuja', 'Kaduna', 'Nassarawa', 'Jos']
+print(city)
+
+city.sort(reverse=True)
+print(city)
+
+city.sort()
+print(city)
+
+city = ['Abuja', 'Kaduna', 'Nassarawa', 'Jos']
+print(len(city))
