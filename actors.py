f60adb80a6fa08928227b7362fd6eedec383744d
ogah samson <ogahsamsonogah@gmail.com>
2026-09-11 16:20:32 +0100

    actors.py

---
+++ actors.py
@@ -0,0 +1,21 @@
+actors = ['jim', 'danzel', 'kason', 'halle']
+actors.sort()
+print(actors)
+
+actors.sort(reverse=True)
+print(actors)
+
+print("Here is the original list:")
+print(actors)
+
+print("\nHere is the sorted list:")
+print(sorted(actors))
+
+print("\nHere is the original list:")
+print(actors)
+
+actors.reverse()
+print(actors)
+
+actors = ['jim', 'danzel', 'kason', 'halle']
+print(len(actors))
