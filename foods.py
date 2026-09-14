b820842d5d4eae716c03455bc68d190e9f0c3bc8
Ogah Samson <ogahsamsonogah@gmail.com>
2026-09-14 21:24:54 +0100

    foods.py

---
+++ foods.py
@@ -0,0 +1,11 @@
+my_foods = ['rice', 'beans', 'indomie', 'yam']
+friends_food = my_foods[:]
+
+my_foods.append('spaghetti')
+friends_food.append('noodles')
+
+print("My favorite foods are")
+print(my_foods)
+
+print("\nMy friends favorite foods are")
+print(friends_food)
