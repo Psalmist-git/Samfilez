0bafd0b296c60597d1a2494ce2077719b1524b27
Samson Ogah <ogahsamsonogah@gmail.com>
2026-09-14 11:41:28 +0100

    intloop.py

---
+++ intloop.py
@@ -0,0 +1,13 @@
+for value in range(1,11):
+    print(value)
+
+numbers = list(range(1,11))
+print(numbers)
+
+even_numbers = list(range(2,11,4))
+print(even_numbers)
+
+squares = []
+for value in range (1,20):
+    square = value**2
+    squares.append(square)
