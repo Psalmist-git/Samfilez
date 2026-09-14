9af47bf20003b48019cd7eb2cc3e9ee53eb1a5ad
Samson Ogah <ogahsamsonogah@gmail.com>
2026-09-14 12:21:06 +0100

    try.py

---
+++ try.py
@@ -0,0 +1,14 @@
+numbers = list(range(1,21))
+print(numbers)
+
+odd_numbers  = list(range(3,30,3))
+print(odd_numbers)
+
+square = []
+for value in range (1,10):
+    square = value**2
+    print(square)
+
+squares = [value**2 for value in range(1,10)]
+print(squares)
+
