3e5585d7295bbd63d2e13551ac88d44cabf2ac7a
Ogah Samson <ogahsamsonogah@gmail.com>
2026-09-14 23:24:11 +0100

    dimensions.py

---
+++ dimensions.py
@@ -0,0 +1,9 @@
+dimensions = (400, 300, 200, 100)
+print("original dimension:")
+for dimension in dimensions:
+    print(dimension)
+
+dimensions = (400, 300, 200, 100)
+print("\nModified dimensions:")
+for dimension in dimensions:
+    print(dimension)
