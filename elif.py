126bf0692bea7380ed59666a89bae622b5d0eb02
Ogah Samson <ogahsamsonogah@gmail.com>
2026-09-16 00:22:45 +0100

    elif.py

---
+++ elif.py
@@ -0,0 +1,12 @@
+age = 18
+if age < 4:
+    print("Your subscription  is fee is free")
+
+elif age < 10:
+    print("Your subscription fee is $5")
+
+elif age < 15:
+    print("Your subsrioption fee is $10")
+
+else:
+    print("Your subscription fee is $20")
