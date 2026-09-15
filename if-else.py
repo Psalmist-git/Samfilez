6f733e341c64c2075f035ce483963c5819db0c7b
ogah samson <ogahsamsonogah@gmail.com>
2026-09-15 15:48:35 +0100

    if-else.py

---
+++ if-else.py
@@ -0,0 +1,7 @@
+age = 17
+if age >= 18:
+    print("Ypu are old enough to vote!")
+    print("Have you registered yourself for the forthcoming elections")
+else:
+    print("You are not eligeable to vote!")
+    print("Please go and get your voters card!")
