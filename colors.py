24e87036511750fd5ac4bdd91a84a649b0390076
Ogah Samson <ogahsamsonogah@gmail.com>
2026-09-16 23:19:03 +0100

    colors.py

---
+++ colors.py
@@ -0,0 +1,11 @@
+alien_color = ['green', 'yellow', 'red']
+if 'green' in alien_color:
+    print("Player has earned 5 points for shooting at t  he alien")
+
+if 'yellow' in alien_color:
+    print("Player has earned 10 points")
+
+elif 'peach' in alien_color:
+    print("Player has earned 10 points")
+
+print("\nLevel passed, Next!")
