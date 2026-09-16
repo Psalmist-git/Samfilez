99691b1a336d5e0e2d02472f3159078ccfeb8eff
Ogah Samson <ogahsamsonogah@gmail.com>
2026-09-16 13:44:43 +0100

    toppings.py

---
+++ toppings.py
@@ -0,0 +1,7 @@
+requested_movies = ['Panther', 'Curse', 'Rings']
+
+for requested_movie in requested_movies:
+    print("Watching a movie called " + requested_movie)
+
+print
+("\nFinished watching the movie!")
