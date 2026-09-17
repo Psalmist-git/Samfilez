be7449900ed39dc87ad24f38013bf8fda68d7904
Ogah Samson <ogahsamsonogah@gmail.com>
2026-09-17 22:29:23 +0100

    dictionary.py

---
+++ dictionary.py
@@ -0,0 +1,9 @@
+club = {
+    'shirt': 'chelsea',
+    'names': ['Cole Palmer', 'Morgan Rogers'],
+    }
+
+print("You  ordered a " + club['shirt'] + " club jersey " + "with the following names:")
+
+for names in club['names']:
+    print("\t" + names)
