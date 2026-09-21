5fc905be4c94cf95fe02012939e71caf6463c4ef
Ogah Samson <ogahsamsonogah@gmail.com>
2026-09-21 11:23:24 +0100

    numerical_input.py

---
+++ numerical_input.py
@@ -0,0 +1,8 @@
+height = input("How tall are you in inches? ")
+height = int(height)
+
+if height >= 35:
+    print("\nYou are tall enough to ride")
+
+else:
+    print("\nYou need to add a little more height.")
