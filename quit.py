e895ff9978ee1bc6a46141bf92adfaad0f170e31
Ogah Samson <ogahsamsonogah@gmail.com>
2026-09-21 15:22:21 +0100

    quit.py

---
+++ quit.py
@@ -0,0 +1,12 @@
+current_number = 1
+while current_number <= 5:
+    print(current_number)
+    current_number = current_number + 1
+
+promt = "\nSay something, i'm giving up on you:"
+promt += "\nEnter 'quit' to end program. "
+
+message = "i am a dog"
+while message !=  'quit':
+    message = input(promt)
+    print(message)
