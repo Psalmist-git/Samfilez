0d350985cea55e514833520f419b5542efe2dbe9
Ogah Samson <ogahsamsonogah@gmail.com>
2026-09-21 17:01:56 +0100

    t.py

---
+++ t.py
@@ -0,0 +1,12 @@
+current_age = 2
+if current_age <= 3:
+    print("\nYour ticket price is free")
+
+    if current_age < 12:
+        print("\nYour ticket fee is $5")
+
+    if current_age >= 18:
+        print("\nYour ticket fee is $10")
+
+    else:
+        print("\nYour ticket price is $15")
