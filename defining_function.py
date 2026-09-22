f607408806e1acee53464b88b28e2ea8deaa73d6
Ogah Samson <ogahsamsonogah@gmail.com>
2026-09-23 00:24:21 +0100

    defining_function.py

--- defining_function.py
+++ defining_function.py
@@ -7,3 +7,11 @@ def favorite_book(hellish):
     print(f"One of my favorite books is {hellish.title()}!")
 
 favorite_book('hellish')
+
+
+def descibe_pet(animal_type, pet_name):
+    print(f"\nI have a {animal_type}.")
+    print(f"My {animal_type}'s name is {pet_name.title()}.")
+
+descibe_pet('dog', 'bosco')
+descibe_pet('cat', 'charlie')
