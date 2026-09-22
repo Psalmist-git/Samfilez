6b9117be1d0b5c1109a3c10ac81966d6289e895e
Ogah Samson <ogahsamsonogah@gmail.com>
2026-09-22 23:43:42 +0100

    defining_function.py

---
+++ defining_function.py
@@ -0,0 +1,9 @@
+def display_message():
+    print("I am learning how to define a function by calling it as well as how to use arguments and parameters!")
+
+display_message()
+
+def favorite_book(hellish):
+    print(f"One of my favorite books is {hellish.title()}!")
+
+favorite_book('hellish')
