586536fd0b9d2a0ea917d93fb125793ba360e110
Ogah Samson <ogahsamsonofag@gmail.com>
2026-09-22 16:07:36 +0100

    sandwich_poll.py

--- sandwich_poll.py
+++ sandwich_poll.py
@@ -11,3 +11,22 @@ while sandwich_orders:
 print("\n--- All Sandwiches Made for our esteemed clients ---")
 for sandwich in finished_sandwiches:
     print(sandwich.title() + " sandwich")
+
+
+sandwich_orders = ['banana', 'flakes', 'jelly', 'flakes', 'buns', 'flakes']
+finished_sandwiches = []
+
+print("\nSorry, we have completely run out of flakes today!")
+
+while 'flakes' in sandwich_orders:
+    sandwich_orders.remove('flakes')
+
+while sandwich_orders:
+    current_sandwichs = sandwich_orders.pop()
+
+    print("I made your " + current_sandwichs + "sandwich.")
+    finished_sandwiches.append(current_sandwichs)
+
+print("\n--- Finito ---")
+for sandwich in finished_sandwiches:
+    print(f"- {sandwich.title()} sandwich")
